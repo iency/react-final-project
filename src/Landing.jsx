0774bc74362bc671a970f0bae71cfae9ae0855e7
Vladimir <vladimir.snegov@outloud.games>
2022-08-03 20:50:58 +0300

    com

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -4,14 +4,11 @@ import './Landing.css';
 const Landing = () => {
   return (
     <div className='landing'>
-      <img 
-      src="https://www.pcgamesn.com/wp-content/uploads/2022/01/best-mmo-games-lost-ark.jpg" 
-      alt="" 
-      />
+        .
         <div className="landing__searchBar">
-          <input className='landing__searchInput' placeholder='Search MMO games...' type="text" />
+          <input className='landing__searchInput' placeholder='Search for your favorite MMO here' type="text" />
         </div>
-    </div>
+      </div>
   );
 }
 
